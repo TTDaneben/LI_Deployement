887c41972fb9aee4cf317036f619ce95a39fd7ae
TTDaneben <163291701+TTDaneben@users.noreply.github.com>
2024-10-13 21:36:01 +0000

    render deployement

--- Inspector_V1/Inspector_V1/settings.py
+++ Inspector_V1/Inspector_V1/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '11ede8ba-b42b-4d70-9c3f-df51c8e06058'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
